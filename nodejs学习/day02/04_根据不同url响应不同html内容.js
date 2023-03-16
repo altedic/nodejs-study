a9055dfa1cd652a513e37e5db2963af967c2fe59
altedic <70313243+altedic@users.noreply.github.com>
2023-03-16 17:48:13 +0800

    Update 04_根据不同url响应不同html内容.js

--- nodejs学习/day02/04_根据不同url响应不同html内容.js
+++ nodejs学习/day02/04_根据不同url响应不同html内容.js
@@ -4,7 +4,7 @@ const server = http.createServer()
 server.on('request', (req, res) => {
     const url = req.url
     let content = '<h1>404 Not Found!</h1>'
-    console.log((url == '/' || url == '/index.html') ? content = '<h1>首页</h1>' : url == '/about.html' ? '<h1>关于页面</h1>' : '<h1>非指定页面</h1>')
+    console.log((url == '/' || url == '/index.html') ? content = '<h1>首页</h1>' : url == '/about.html' ? '<h1>关于页面</h1>' : content)
 
     res.setHeader('Content-Type', 'test/html: charset=utf-8')
 
